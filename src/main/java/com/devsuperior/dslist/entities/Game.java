1b96b25e668649f6d6868258836700842ef57eb9
Lucas Pires <contato.lucaspires93@gmail.com>
2025-08-19 15:57:57 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -22,24 +22,30 @@ public class Game {
 	@Column(name ="game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition= "TEXT")
 	private String shortDescription;
-	private String longDescreiption;
+	
+	@Column(columnDefinition= "TEXT")
+	private String longDescription;
 	
 	public Game() {
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescreiption) {
+	public Game(Long id, String title, Integer year, String genre, String platforms,Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score= score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescreiption = longDescreiption;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -75,11 +81,19 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platform;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -99,11 +113,11 @@ public class Game {
 	}
 
 	public String getLongDescreiption() {
-		return longDescreiption;
+		return longDescription;
 	}
 
 	public void setLongDescreiption(String longDescreiption) {
-		this.longDescreiption = longDescreiption;
+		this.longDescription = longDescreiption;
 	}
 
 	@Override
